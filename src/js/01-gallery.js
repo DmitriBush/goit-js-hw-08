816b06d431285480d0c362b4282cc70c61c82218
DmitriiBush <dmitriybush77@gmail.com>
2023-02-10 17:58:12 +0100

    save

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const imageMarcup = createImageCardMarcup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imageMarcup);
+
+// console.log(imageMarcup);
+
+function createImageCardMarcup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+             <img
+               class="gallery__image"
+               src="${preview}"
+               
+               alt="${description}"
+              />
+            </a>
+        </div>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+let formData = {};
+
+form.addEventListener('input', throttle(onInputForm, 1000));
+form.addEventListener('submit', submitForm);
+
+function onInputForm(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function dataCheck() {
+  let savedDataForm = localStorage.getItem(LOCALSTORAGE_KEY);
+
+  if (savedDataForm) {
+    savedDataForm = JSON.parse(savedDataForm);
+
+    Object.entries(savedDataForm).forEach(([name, value]) => {
+      formData[name] = value;
+      form.elements[name].value = value;
+    });
+  }
+}
+
+dataCheck();
+
+function submitForm(evt) {
+  evt.preventDefault();
+
+  if (form.email.value === '' || form.message.value === '') {
+    alert('Fill in the fields!');
+    return;
+  }
+
+  formData = {};
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
